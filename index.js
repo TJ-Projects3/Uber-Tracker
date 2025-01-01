61c3d9234ebfd7bddee35b271da161a0ade296b1
TJ-Projects3 <tjprojects3@gmail.com>
2025-01-01 14:22:58 -0500

    Added save, increment, and tracking functions, updates to frontend

---
+++ index.js
@@ -0,0 +1,18 @@
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    if (count == 0) {
+         return alert("Update the count.")
+    }
+    passangerCount = count + " ~ "
+    saveEl.textContent += passangerCount
+    countEl.textContent = 0
+    count = 0
+}
